144481dfba01f5e2bacc51ce92608e91342acfa1
jason13official <jason13official@gmail.com>
2025-06-03 15:10:23 -0400

    fix some things

--- src/main/java/com/cursee/examplemod/Constants.java
+++ src/main/java/com/cursee/examplemod/Constants.java
@@ -5,5 +5,5 @@ import org.slf4j.Logger;
 
 public class Constants {
     public static final String MOD_ID = "examplemod";
-    private static final Logger LOG = LogUtils.getLogger();
+    public static final Logger LOG = LogUtils.getLogger();
 }
